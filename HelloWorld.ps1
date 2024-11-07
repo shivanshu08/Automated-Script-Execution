7eb46f694cb7a22cea0a98199f9431c4d29ae296
Shivanshu Dwivedi <128125718+shivanshu08@users.noreply.github.com>
2024-11-07 23:30:55 +0530

    Update HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,16 +1,11 @@
 $directoryPath = "./Logs/"
 $currentDateTime = Get-Date -Format "MM-dd-yyyy_HH-mm-ss"
 $logFileName = "$directoryPath/Logs_$currentDateTime.txt"
-param (
-    [string]$logFile
-)
 
 for ($i = 1; $i -le 10; $i++) {
     $message = "Hello World!"
     Write-Output $message
     Add-Content -Path $logFileName -Value $message
-    Add-Content -Path $logFile -Value $message
 }
 
 Write-Output "Log file saved to $logFileName"
-Write-Output "Log file saved to $logFile"
