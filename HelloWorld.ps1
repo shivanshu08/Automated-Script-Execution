3822a98399d7a862a89a9c0b4c4687f0c42783ca
Shivanshu Dwivedi <128125718+shivanshu08@users.noreply.github.com>
2024-11-07 22:29:37 +0530

    Update HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,7 +1,13 @@
-$counter = 0
+$directoryPath = "./Logs/"
+$currentDateTime = Get-Date -Format "MM-dd-yyyy_HH-mm-ss"
+$logFileName = "$directoryPath/Logs_$currentDateTime.txt"
 
-# Loop 5 times
-while ($counter -lt 5) {
-    Write-Host "Hello User!"
-    $counter++
+for ($i = 1; $i -le 10; $i++) {
+    $message = "Hello World!"
+    Write-Output $message
+    Add-Content -Path $logFileName -Value $message
 }
+
+Write-Output "Log file saved to $logFileName"
+
+name: Run Azure Activity PowerShell Script
