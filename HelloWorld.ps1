8be341bfbbbde6d8d8b16f783fcd78900371bff9
Shivanshu Dwivedi <128125718+shivanshu08@users.noreply.github.com>
2024-11-07 23:34:25 +0530

    Update HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -3,7 +3,7 @@ $currentDateTime = Get-Date -Format "MM-dd-yyyy_HH-mm-ss"
 $logFileName = "$directoryPath/Logs_$currentDateTime.txt"
 
 for ($i = 1; $i -le 5; $i++) {
-    $message = "Hello User👋!"
+    $message = "Hello User👋"
     Write-Output $message
     Add-Content -Path $logFileName -Value $message
 }
